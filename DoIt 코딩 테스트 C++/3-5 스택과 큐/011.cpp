4a9a2a4830a1e79469aceed8bb275a3fd9f4d982
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2023-08-03 17:07:05 +0900

    23.08.03

---
+++ DoIt 코딩 테스트 C++/3-5 스택과 큐/011.cpp
@@ -0,0 +1,75 @@
+// 백준 1874 번 / 스택으로 수열 만들기
+
+#include <iostream>
+#include <vector>
+#include <stack> // cpp 스택 헤더 사용
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    // 수열 저장 vector
+    vector<int> A(N, 0);
+    // 출력 vector
+    vector<char> resultV;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    stack<int> stack;
+    int num = 1;
+    // 출력 여부 플래그 변수
+    bool out = true;
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        int e = A[i]; // 현재 수열 원소
+
+        if (e >= num)
+        {
+            while (e >= num)
+            {
+                stack.push(num++);      // 후위연산
+                resultV.push_back('+'); // push
+            }
+
+            // 스택 맨 위에 e 가 push됨
+            stack.pop();
+            resultV.push_back('-');
+        }
+
+        // e < num , 이미 스택 안에 e가 있는 상태
+        else
+        {
+            int top = stack.top();
+
+            // top이 현재 수열 원소보다 커져버리면 아래 값을 뺄 수 없다, 후입선출 원리
+            if (top > e)
+            {
+                cout << "NO";
+                out = false;
+                break;
+            }
+
+            // top == e
+            else
+            {
+                stack.pop();
+                resultV.push_back('-');
+            }
+        }
+    }
+
+    // 출력
+    if (out)
+    {
+        for (int i = 0; i < resultV.size(); i++)
+        {
+            cout << resultV[i] << '\n';
+        }
+    }
+}
---
+++ DoIt 코딩 테스트 C++/3-5 스택과 큐/012.cpp
@@ -0,0 +1,50 @@
+// 백준 17298번 / 오큰수 구하기
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> A(N, 0);
+    vector<int> ans(N, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    // 스택에는 인덱스 번호를 저장
+    stack<int> stack;
+    stack.push(0);
+
+    for (int i = 1; i < N; i++)
+    {
+
+        // pop 해가면서 오큰수 업데이트
+        while (!stack.empty() && A[stack.top()] < A[i])
+        {
+            ans[stack.top()] = A[i];
+            stack.pop();
+        }
+        // 현재 인덱스 스택에 저장
+        stack.push(i);
+    }
+
+    // 반복문 종료 후 스택에 남아있는 인덱스?
+    // 오큰수가 없는 경우
+    while (!stack.empty())
+    {
+
+        ans[stack.top()] = -1;
+        stack.pop();
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << ans[i] << " ";
+    }
+}
